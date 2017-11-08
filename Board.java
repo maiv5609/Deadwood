6164c73f6a10c6db91b33dda142861c4b640cbc8
barretj5 <barretj5@wwu.edu>
2017-11-07 16:55:58 -0800

    Started constructor for Board

--- Board.java
+++ Board.java
@@ -4,7 +4,16 @@ import java.util.Map;
 public class Board {
 	Map<Integer, Room> roomMap;
 	List<Scene> scenes;
+	Integer topOfDeck;
 
+	Board() {
+		topOfDeck = 0;
+		for (Integer i = 0; i < 4; i++){
+			Room curr = new Room();
+			roomMap.put(i, curr);
+		}
+		// Setup scene list
+	}
 	public Map<Integer, Room> getRoomMap() {
 		return roomMap;
 	}
@@ -20,4 +29,5 @@ public class Board {
 	public void setScenes(List<Scene> scenes) {
 		this.scenes = scenes;
 	}
+
 }
