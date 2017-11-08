e0a4690c22cfcccb878362c06e080f58c13d854f
AlexandraPuchko <alexandrapuchko@wlan010-111-059-217.wlan.wwu.edu>
2017-11-07 17:00:47 -0800

    Room and Game updates

--- Game.java
+++ Game.java
@@ -1,85 +1,139 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class Game {
-	int turn;
-	int currDay;
-	int maxDay;
-	List<Player> players;
-	int roomsRemaining;
-	int totalRooms;
-
-	public int getTurn() {
-		return turn;
-	}
-
-	public void setTurn(int turn) {
-		this.turn = turn;
-	}
-
-	public int getCurrDay() {
-		return currDay;
-	}
-
-	public void setCurrDay(int currDay) {
-		this.currDay = currDay;
-	}
-
-	public int getMaxDay() {
-		return maxDay;
-	}
-
-	public void setMaxDay(int maxDay) {
-		this.maxDay = maxDay;
-	}
-
-	public List<Player> getPlayers() {
-		return players;
-	}
-
-	public void setPlayers(List<Player> players) {
-		this.players = players;
-	}
-
-	public int getRoomsRemaining() {
-		return roomsRemaining;
-	}
-
-	public void setRoomsRemaining(int roomsRemaining) {
-		this.roomsRemaining = roomsRemaining;
-	}
-
-	public int getTotalRooms() {
-		return totalRooms;
-	}
-
-	public void setTotalRooms(int totalRooms) {
-		this.totalRooms = totalRooms;
-	}
-
-	private void nextDay() {
-
-	}
-
-	private void scoring() {
-
-	}
-
-	private void startGame() {
-
-	}
-
-	private void endGame() {
-
-	}
-
-	public void handleInput(String action, String parameters) {
-		// Player currentPlayer = getPlayers().get(getTurn());
-		// currentPlayer.handleAction(action, parameters);
-	}
-
-	public static void main(String[] args) {
-
-		System.out.print("It works!");
-	}
-
+    int turn;
+    int currDay;
+    int maxDay;
+    List<Player> players;
+    int roomsRemaining;
+    int totalRooms;
+    
+    public int getTurn() {
+        return turn;
+    }
+    
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
+    
+    public int getCurrDay() {
+        return currDay;
+    }
+    
+    public void setCurrDay(int currDay) {
+        this.currDay = currDay;
+    }
+    
+    public int getMaxDay() {
+        return maxDay;
+    }
+    
+    public void setMaxDay(int maxDay) {
+        this.maxDay = maxDay;
+    }
+    
+    public List<Player> getPlayers() {
+        return players;
+    }
+    
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+    
+    public int getRoomsRemaining() {
+        return roomsRemaining;
+    }
+    
+    public void setRoomsRemaining(int roomsRemaining) {
+        this.roomsRemaining = roomsRemaining;
+    }
+    
+    public int getTotalRooms() {
+        return totalRooms;
+    }
+    
+    public void setTotalRooms(int totalRooms) {
+        this.totalRooms = totalRooms;
+    }
+    
+    private void nextDay() {
+        
+    }
+    
+    private void scoring() {
+        List<Player> players = this.players;
+        if(players != null & !players.isEmpty()){
+            for (Player player : players){
+                player.setTotal(player.getCredits() + player.getMoney() + 5*player.getRank());
+            }
+        }
+    }
+    
+    private void startGame(boolean b, int currentDay, int turn, int maxDay, int playersNum, int maxDays) {
+        //create a Trailer
+        Room currentRoom = new Room();
+        currentRoom.setTrailer(b);
+        
+        //add players
+        for (int i = 0; i < 3; i++){
+            Player player = new Player(0,i,0,0,currentRoom);
+            this.players.add(player);
+        }
+        //set Current day to 1
+        this.currDay = currentDay;
+        //set Current turn to 0
+        this.turn = turn;
+        //set Max Day to 0
+        this.maxDay = maxDay;
+    }
+    
+    private void endGame() {
+        
+    }
+    
+    public void handleUserInput(String action, String parameters) {
+        // Player currentPlayer = getPlayers().get(getTurn());
+        // currentPlayer.handleAction(action, parameters);
+    }
+    
+    
+    
+    public void updateView(){
+        
+    }
+    
+    
+    private static String inputReader(){
+        String input = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            input = br.readLine();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return input;
+    }
+    
+    public static void main(String[] args) {
+        //ask user for players number
+        System.out.println("Please, set number of players:");
+        int playersNum = Integer.parseInt(inputReader());
+        //ask user for number of days
+        System.out.println("Please, set number of days:");
+        int maxDays = Integer.parseInt(inputReader());
+        
+        //start game with initial params
+        Game game = new Game();
+        game.startGame(true, 1, 1, 4, playersNum, maxDays);
+        
+        System.out.print("The game has just started!!");
+        
+        
+        
+    }
+    
 }
--- Room.java
+++ Room.java
@@ -2,106 +2,96 @@ import java.util.List;
 import java.util.Map;
 
 public class Room {
-
-	boolean isCastingOffice;
-	boolean isTrailers;
-	int maxShots;
-	int currentShots;
-	List<Integer> playersInTheRoom;
-	List<Role> roles;
-	Scene scene;
-	int roomType;
-	Map<Integer, Room> connectedNodes;
-	String orientation; // horizontal or vertical
-
-	public boolean isCastingOffice() {
-		return isCastingOffice;
-	}
-
-	public void setCastingOffice(boolean isCastingOffice) {
-		this.isCastingOffice = isCastingOffice;
-	}
-
-	public boolean isTrailers() {
-		return isTrailers;
-	}
-
-	public void setTrailers(boolean isTrailers) {
-		this.isTrailers = isTrailers;
-	}
-
-	public int getMaxShots() {
-		return maxShots;
-	}
-
-	public void setMaxShots(int maxShots) {
-		this.maxShots = maxShots;
-	}
-
-	public int getCurrentShots() {
-		return currentShots;
-	}
-
-	public void setCurrentShots(int currentShots) {
-		this.currentShots = currentShots;
-	}
-
-	public List<Integer> getPlayersInTheRoom() {
-		return playersInTheRoom;
-	}
-
-	public void setPlayersInTheRoom(List<Integer> playersInTheRoom) {
-		this.playersInTheRoom = playersInTheRoom;
-	}
-
-	public List<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(List<Role> roles) {
-		this.roles = roles;
-	}
-
-	public Scene getScene() {
-		return scene;
-	}
-
-	public void setScene(Scene scene) {
-		this.scene = scene;
-	}
-
-	public int getRoomType() {
-		return roomType;
-	}
-
-	public void setRoomType(int roomType) {
-		this.roomType = roomType;
-	}
-
-	public Map<Integer, Room> getConnectedNodes() {
-		return connectedNodes;
-	}
-
-	public void setConnectedNodes(Map<Integer, Room> connectedNodes) {
-		this.connectedNodes = connectedNodes;
-	}
-
-	public void addPlayer() {
-
-	}
-
-	public void removePlayer() {
-
-	}
-
-	public List<Room> listAdjacentRooms() {
-		List<Room> room = null;
-		;
-		return room;
-	}
-
-	public void updateShots() {
-
-	}
-
+    
+    private boolean isCastingOffice;
+    private boolean isTrailer;
+    private int maxShots;
+    private int currentShots;
+    private List<Integer> playersInTheRoom;
+    private List<Role> roles;
+    private Scene scene;
+    private Map<Integer, Room> connectedNodes;
+    private String roomType; // horizontal or vertical
+    
+    public boolean isCastingOffice() {
+        return isCastingOffice;
+    }
+    
+    public void setCastingOffice(boolean isCastingOffice) {
+        this.isCastingOffice = isCastingOffice;
+    }
+    
+    public boolean isTrailer() {
+        return isTrailer;
+    }
+    
+    public void setTrailer(boolean isTrailer) {
+        this.isTrailer = isTrailer;
+    }
+    
+    public int getMaxShots() {
+        return maxShots;
+    }
+    
+    public void setMaxShots(int maxShots) {
+        this.maxShots = maxShots;
+    }
+    
+    public int getCurrentShots() {
+        return currentShots;
+    }
+    
+    public void setCurrentShots(int currentShots) {
+        this.currentShots = currentShots;
+    }
+    
+    public List<Integer> getPlayersInTheRoom() {
+        return playersInTheRoom;
+    }
+    
+    public void setPlayersInTheRoom(List<Integer> playersInTheRoom) {
+        this.playersInTheRoom = playersInTheRoom;
+    }
+    
+    public List<Role> getRoles() {
+        return roles;
+    }
+    
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+    
+    public Scene getScene() {
+        return scene;
+    }
+    
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+    
+    public Map<Integer, Room> getConnectedNodes() {
+        return connectedNodes;
+    }
+    
+    public void setConnectedNodes(Map<Integer, Room> connectedNodes) {
+        this.connectedNodes = connectedNodes;
+    }
+    
+    public String getRoomType() {
+        return roomType;
+    }
+    
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+    public void addPlayer(int playerNumber) {
+        this.playersInTheRoom.add(playerNumber);
+    }
+    
+    public void removePlayer(int playerNumber) {
+        this.playersInTheRoom.remove(playerNumber);
+    }
+    
+    
+    
 }
