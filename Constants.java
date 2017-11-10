9cbc6cfd9a2f22107f16c6452c16d06f77c31718
AlexandraPuchko <alexandrapuchko@wlan010-111-059-217.wlan.wwu.edu>
2017-11-10 13:42:19 -0800

    class Constants with action names

---
+++ Constants.java
@@ -0,0 +1,15 @@
+
+public class Constants {
+	
+	//prevent instantiation
+	private Constants(){}
+	
+	public static final String WORK = "work";
+	public static final String MOVE = "move";
+	public static final String TAKE_ROLE = "take role";
+	public static final String UPGRADE_RANK = "upgrade rank";
+	public static final String REHEARSE = "rehearse";
+	
+	
+
+}
