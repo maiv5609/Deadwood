8148f3d806799a1f72e20f803a59aeaea4ecf584
maiv <maiv@wwu.edu>
2017-11-09 20:38:41 -0800

    minor changes to indents and attributes

--- Scene.java
+++ Scene.java
@@ -2,51 +2,51 @@ import java.util.List;
 
 public class Scene {
 
-	String name;
-	int budget;
-	List<Role> role;
-	Boolean isClosed;
-	Boolean isOccupied;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getBudget() {
-		return budget;
-	}
-
-	public void setBudget(int budget) {
-		this.budget = budget;
-	}
-
-	public List<Role> getRole() {
-		List<Role> role = null;
-		return role;
-	}
-
-	public void setRole(List<Role> role) {
-		this.role = role;
-	}
-
-	public Boolean getIsClosed() {
-		return isClosed;
-	}
-
-	public void setIsClosed(Boolean isClosed) {
-		this.isClosed = isClosed;
-	}
-
-	public Boolean getIsOccupied() {
-		return isOccupied;
-	}
-
-	public void setIsOccupied(Boolean isOccupied) {
-		this.isOccupied = isOccupied;
-	}
-
+    String name;
+    String description;
+    int budget;
+    List<Role> role;
+    Boolean isClosed;
+    Boolean isOccupied;
+    
+    public String getName() {
+	return name;
+    }
+    
+    public void setName(String name) {
+	this.name = name;
+    }
+    
+    public int getBudget() {
+	return budget;
+    }
+    
+    public void setBudget(int budget) {
+	this.budget = budget;
+    }
+    
+    public List<Role> getRole() {
+	List<Role> role = null;
+	return role;
+    }
+    
+    public void setRole(List<Role> role) {
+	this.role = role;
+    }
+    
+    public Boolean getIsClosed() {
+	return isClosed;
+    }
+    
+    public void setIsClosed(Boolean isClosed) {
+	this.isClosed = isClosed;
+    }
+    
+    public Boolean getIsOccupied() {
+	return isOccupied;
+    }
+    
+    public void setIsOccupied(Boolean isOccupied) {
+	this.isOccupied = isOccupied;
+    }    
 }
